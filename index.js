80da03551bd4ad81916e2f5c9d82155c0012c42d
ekoyudhi <ekoyudhiprastowo@gmail.com>
2023-01-10 17:00:45 +0700

    Test with Pug

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+//app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+// })
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.render('index');
 })
 
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`The application started on port ${server.address().port}`);
+});
 
 module.exports = app;

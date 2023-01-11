e450d6779d15027c1966afcb40681937e1526809
ekoyudhi <ekoyudhiprastowo@gmail.com>
2023-01-11 09:24:58 +0700

    update tailwind

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 //app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/', (req, res) => {
 //     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
